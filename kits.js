8179f8ee8436e48909914dacd30653ceb19d833b
yuzijiang <75778540@qq.com>
2019-08-20 19:45:11 +0800

    Create kits.js

---
+++ kits.js
@@ -0,0 +1,50 @@
+
+
+var kits = {};
+
+// 1.求n-m的和
+kits.dispatchZero = function (n,m) {
+  var num = 0;
+  for(let i = n;i <= m;i++){
+     num = num + i;
+  }
+  return num;
+}
+
+// 2.获取当前的北京时间 
+
+// 把方法都放到对象的身上
+kits.formatDate = function () {
+  var date = new Date();
+  // 把年月日时分秒获取
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  month = this.dispatchZero(month);
+  var day = date.getDate();
+  day = this.dispatchZero(day);
+  var hour = date.getHours();
+  hour = this.dispatchZero(hour);
+  var minute = this.dispatchZero(date.getMinutes());
+  var second = this.dispatchZero(date.getSeconds());
+  return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+}
+
+// 3.获取随机整数
+kits.randomInt = function(n,m){
+  return Math.floor(Math.random() * (m-n+1) + n);
+}
+
+
+
+// 4.常见的给id的方式1
+// 当前时间戳 + 大的随机数
+kits.getId = function(){
+  // 返回一个不容易重复的id
+  let date = new Date();
+  let time = date.getTime();// 得到的是从1970年1月1日到现在为止的毫秒总数
+  // 然后在得到一个足够大的随机数，把毫秒和随机数相连，作为新的id
+  let r = this.randomInt(100000,999999);
+  // 把两个数字连起来
+  let id = time + '' + r;
+  return id;
+}
